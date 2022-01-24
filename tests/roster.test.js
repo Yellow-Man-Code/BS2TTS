b04503ef5100df6443461383946287d819cae561
Gabriel Laskar <gabriel.laskar@gmail.com>
2022-01-24 18:58:15 +0100

    test: add sample test for special cases

--- tests/roster.test.js
+++ tests/roster.test.js
@@ -25,3 +25,93 @@ test("parse roster", () => {
 
     spy.mockRestore();
 });
+
+test("celestine", () => {
+    var id = 0
+    const spy = jest.spyOn(crypto, "randomBytes").mockImplementation((size) => {
+        return Buffer.alloc(size, id++);
+    });
+
+    const fileContent = fs.readFileSync(path.join(__dirname, "../samples", "adepta-sororitas-celestine.rosz"));
+
+    const roster = roszParse(fileContent);
+
+    approvals.verify(__dirname, getTestNameSafe(), roszParser.serialize(roster, 4))
+
+    spy.mockRestore();
+})
+
+test("tau commander", () => {
+    var id = 0
+    const spy = jest.spyOn(crypto, "randomBytes").mockImplementation((size) => {
+        return Buffer.alloc(size, id++);
+    });
+
+    const fileContent = fs.readFileSync(path.join(__dirname, "../samples", "tau-commander.rosz"));
+
+    const roster = roszParse(fileContent);
+
+    approvals.verify(__dirname, getTestNameSafe(), roszParser.serialize(roster, 4))
+
+    spy.mockRestore();
+});
+
+test("vanguards", () => {
+    var id = 0
+    const spy = jest.spyOn(crypto, "randomBytes").mockImplementation((size) => {
+        return Buffer.alloc(size, id++);
+    });
+
+    const fileContent = fs.readFileSync(path.join(__dirname, "../samples", "sample-sm-vanguard-vets.rosz"));
+
+    const roster = roszParse(fileContent);
+
+    approvals.verify(__dirname, getTestNameSafe(), roszParser.serialize(roster, 4))
+
+    spy.mockRestore();
+});
+
+test("sm bike squad", () => {
+    var id = 0
+    const spy = jest.spyOn(crypto, "randomBytes").mockImplementation((size) => {
+        return Buffer.alloc(size, id++);
+    });
+
+    const fileContent = fs.readFileSync(path.join(__dirname, "../samples", "sample-sm-bike-squad.rosz"));
+
+    const roster = roszParse(fileContent);
+
+    approvals.verify(__dirname, getTestNameSafe(), roszParser.serialize(roster, 4))
+
+    spy.mockRestore();
+});
+
+test("blood angel librarian dreadnought", () => {
+    var id = 0
+    const spy = jest.spyOn(crypto, "randomBytes").mockImplementation((size) => {
+        return Buffer.alloc(size, id++);
+    });
+
+    const fileContent = fs.readFileSync(path.join(__dirname, "../samples", "sample-sm-librarian-dreadnought.rosz"));
+
+    const roster = roszParse(fileContent);
+
+    approvals.verify(__dirname, getTestNameSafe(), roszParser.serialize(roster, 4))
+
+    spy.mockRestore();
+});
+
+test("grey knight land raider", () => {
+    var id = 0
+    const spy = jest.spyOn(crypto, "randomBytes").mockImplementation((size) => {
+        return Buffer.alloc(size, id++);
+    });
+
+    const fileContent = fs.readFileSync(path.join(__dirname, "../samples", "sample-grey-knights-land-raider.rosz"));
+
+    const roster = roszParse(fileContent);
+
+    approvals.verify(__dirname, getTestNameSafe(), roszParser.serialize(roster, 4))
+
+    spy.mockRestore();
+});
